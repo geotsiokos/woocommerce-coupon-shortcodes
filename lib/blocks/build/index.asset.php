08c4d3268a21b59ed94322734ea59728e573c2a8
George Tsiokos <gtsiokos@gmail.com>
2021-09-07 11:02:38 +0300

    fixed coupon selection on the block settings

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ae3503b52fbfa41dd8b720ced769aecf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1235434be337bc3447142d241958c09c');
