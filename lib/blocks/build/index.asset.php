bd5a2e64efeaccc74a3a4e2218e3289f4433af74
George Tsiokos <gtsiokos@gmail.com>
2021-09-07 10:56:28 +0300

    updating the coupon selection in the block settings

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '79d97075009e064ff59250de364710e4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ae3503b52fbfa41dd8b720ced769aecf');
