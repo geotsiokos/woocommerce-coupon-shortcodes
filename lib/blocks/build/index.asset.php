28231b37fd361febb5bfafee776ff058383c531b
gtsiokos <gtsiokos@gmail.com>
2021-09-08 18:17:39 +0300

    update

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5e271ff3b07692a0ba870a96dc872f36');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9d03868fa6f14e0f9efb0ecab57baf9d');
